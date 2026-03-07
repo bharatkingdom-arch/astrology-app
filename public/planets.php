3aa4d587505d85c4ac29fe9b56ce1be80a6c0a6e
Bharatkumar <bharatkingdom@gmail.com>
2026-03-07 20:36:03 +0530

    compile swetest on railway

--- public/planets.php
+++ public/planets.php
@@ -2,6 +2,14 @@
 
 echo "<h2>Swiss Ephemeris Test</h2>";
 
+$compile = "cd /app/swisseph && make clean && make swetest 2>&1";
+exec($compile, $compile_output);
+
+echo "<h3>Compile output</h3>";
+echo "<pre>";
+print_r($compile_output);
+echo "</pre>";
+
 $cmd = "/app/swisseph/swetest -p0123456789 -eswe -b1.1.2024 -ut0:0 2>&1";
 
 $output = [];
@@ -9,6 +17,7 @@ $return = 0;
 
 exec($cmd, $output, $return);
 
+echo "<h3>Swetest output</h3>";
 echo "<pre>";
 print_r($output);
 echo "</pre>";
