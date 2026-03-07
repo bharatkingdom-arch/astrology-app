c63698542df4192708518645fffac61209f08384
Bharatkumar <bharatkingdom@gmail.com>
2026-03-07 20:21:41 +0530

    compile swetest

--- public/planets.php
+++ public/planets.php
@@ -2,26 +2,17 @@
 
 echo "<h2>Swiss Ephemeris Test</h2>";
 
-$swetest = __DIR__ . "/../swisseph/swetest";
-
-/* compile swetest if not exists */
-if (!file_exists($swetest)) {
-    echo "<b>Compiling Swiss Ephemeris...</b><br>";
-    shell_exec("cd ../swisseph && make swetest");
-}
-
-/* run swetest */
-$cmd = "$swetest -edir ../ephemeris -b1.1.2000 -p0123456789 -fPl";
+$cmd = "cd /app/swisseph && make swetest 2>&1";
 
 $output = [];
 $return = 0;
 
-exec($cmd . " 2>&1", $output, $return);
+exec($cmd, $output, $return);
 
 echo "<pre>";
 print_r($output);
 echo "</pre>";
 
-echo "<br>Return code: $return";
+echo "Return code: $return";
 
 ?>
