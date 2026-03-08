0179f86fdddfac425b42a91e192b8d0003973364
Bharatkumar <bharatkingdom@gmail.com>
2026-03-08 16:34:33 +0530

    auto-commit

--- public/planets.php
+++ public/planets.php
@@ -1,3 +1,20 @@
 <?php
-echo "PHP is working on Railway";
+
+echo "<h2>Swiss Ephemeris Test</h2>";
+
+$swetest = __DIR__ . "/../swisseph/swetest";
+
+$cmd = "$swetest -edir=" . __DIR__ . "/../ephemeris -b1.1.2000 -p0123456789 -fPl";
+
+$output = [];
+$return = 0;
+
+exec($cmd . " 2>&1", $output, $return);
+
+echo "<pre>";
+print_r($output);
+echo "</pre>";
+
+echo "<br>Return code: $return";
+
 ?>
