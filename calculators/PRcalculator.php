b1127dceb248a63f08648742bd59dcbb827ae8e9
Bharatkumar <bharatkingdom@gmail.com>
2026-03-11 18:12:57 +0530

    remove BOM PRcalculator

--- calculators/PRcalculator.php
+++ calculators/PRcalculator.php
@@ -1,8 +1,10 @@
 
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-require '../engine/init.php';
 
+session_start();
 require_once '../engine/SunriseSunset.php';
 require_once '../engine/MuhurthaLagna.php';
 
