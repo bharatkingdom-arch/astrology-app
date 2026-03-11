e5201b52e3bca04c78f91a4aea27bbb70f4b21d9
Bharatkumar <bharatkingdom@gmail.com>
2026-03-11 17:25:16 +0530

    all pr dasa calculations

--- calculators/PRcalculator.php
+++ calculators/PRcalculator.php
@@ -1,10 +1,14 @@
 
 <?php
+ob_start(); // Prevent header errors
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-session_start();
 require_once '../engine/SunriseSunset.php';
 require_once '../engine/MuhurthaLagna.php';
 
