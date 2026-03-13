ef76c164c3dbdea8256a85f3662355eeed4fb93b
Bharatkumar <bharatkingdom@gmail.com>
2026-03-13 21:48:08 +0530

    Added data base

--- engine/db.php
+++ engine/db.php
@@ -1,12 +1,12 @@
 <?php
 
-$host = "localhost";
-$db   = "astrology";
-$user = "root";
-$pass = "password";
+$host = "34.93.xx.xx"; // Cloud SQL public IP
+$user = "astro_user";
+$password = "Astro@123456";
+$database = "astrology";
 
-$conn = new mysqli($host,$user,$pass,$db);
+$conn = new mysqli($host, $user, $password, $database);
 
-if($conn->connect_error){
-    die("DB connection failed");
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
