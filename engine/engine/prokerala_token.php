c72e6b8f0e1efdd6fb3fd4a2e7f3efcd3cde3cfd
Bharatkumar <bharatkingdom@gmail.com>
2026-03-14 11:45:53 +0530

    Added prokeral

--- engine/engine/prokerala_token.php
+++ engine/engine/prokerala_token.php
@@ -6,21 +6,26 @@ $client_secret = "Ddv4lm4mqa5rHsahW3olQhvFeC8ZxUs60e7ekOkp";
 $url = "https://api.prokerala.com/token";
 
 $data = [
-"grant_type" => "client_credentials",
-"client_id" => $client_id,
-"client_secret" => $client_secret
+    "grant_type" => "client_credentials",
+    "client_id" => $client_id,
+    "client_secret" => $client_secret
 ];
 
 $options = [
-"http" => [
-"header" => "Content-Type: application/x-www-form-urlencoded",
-"method" => "POST",
-"content" => http_build_query($data)
-]
+    "http" => [
+        "header" => "Content-Type: application/x-www-form-urlencoded\r\n",
+        "method" => "POST",
+        "content" => http_build_query($data),
+        "ignore_errors" => true
+    ]
 ];
 
 $context = stream_context_create($options);
 
-$response = file_get_contents($url,false,$context);
+$response = file_get_contents($url, false, $context);
 
-echo $response;
+if ($response === FALSE) {
+    echo "Error requesting token.";
+} else {
+    echo $response;
+}
