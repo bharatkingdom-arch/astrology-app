c3238a56129d147be567af210ed879d0bcead4fb
Bharatkumar <bharatkingdom@gmail.com>
2026-05-20 21:02:14 +0530

    Fix all changes

---
+++ translation_script.php
@@ -0,0 +1,15 @@
+<?php
+$zodiac_data = [
+    'aries' => ['name_en' => 'Aries (Mesha)', 'name_te' => 'మేష రాశి (Aries)', 'date' => 'Mar 21 - Apr 19', 'symbol' => '♈'],
+    'taurus' => ['name_en' => 'Taurus (Vrishabha)', 'name_te' => 'వృషభ రాశి (Taurus)', 'date' => 'Apr 20 - May 20', 'symbol' => '♉'],
+    'gemini' => ['name_en' => 'Gemini (Mithuna)', 'name_te' => 'మిథున రాశి (Gemini)', 'date' => 'May 21 - Jun 20', 'symbol' => '♊'],
+    'cancer' => ['name_en' => 'Cancer (Karka)', 'name_te' => 'కర్కాటక రాశి (Cancer)', 'date' => 'Jun 21 - Jul 22', 'symbol' => '♋'],
+    'leo' => ['name_en' => 'Leo (Simha)', 'name_te' => 'సింహ రాశి (Leo)', 'date' => 'Jul 23 - Aug 22', 'symbol' => '♌'],
+    'virgo' => ['name_en' => 'Virgo (Kanya)', 'name_te' => 'కన్య రాశి (Virgo)', 'date' => 'Aug 23 - Sep 22', 'symbol' => '♍'],
+    'libra' => ['name_en' => 'Libra (Tula)', 'name_te' => 'తులా రాశి (Libra)', 'date' => 'Sep 23 - Oct 22', 'symbol' => '♎'],
+    'scorpio' => ['name_en' => 'Scorpio (Vrischika)', 'name_te' => 'వృశ్చిక రాశి (Scorpio)', 'date' => 'Oct 23 - Nov 21', 'symbol' => '♏'],
+    'sagittarius' => ['name_en' => 'Sagittarius (Dhanu)', 'name_te' => 'ధనుస్సు రాశి (Sagittarius)', 'date' => 'Nov 22 - Dec 21', 'symbol' => '♐'],
+    'capricorn' => ['name_en' => 'Capricorn (Makara)', 'name_te' => 'మకర రాశి (Capricorn)', 'date' => 'Dec 22 - Jan 19', 'symbol' => '♑'],
+    'aquarius' => ['name_en' => 'Aquarius (Kumbha)', 'name_te' => 'కుంభ రాశి (Aquarius)', 'date' => 'Jan 20 - Feb 18', 'symbol' => '♒'],
+    'pisces' => ['name_en' => 'Pisces (Meena)', 'name_te' => 'మీన రాశి (Pisces)', 'date' => 'Feb 19 - Mar 20', 'symbol' => '♓']
+];
