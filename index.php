58321e03585d54651a97769496c08dacc7923c2e
Bharatkumar <bharatkingdom@gmail.com>
2026-05-20 18:49:23 +0530

    Fix headers already sent in index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-?>
-
-
-
-<?php require 'header.php'; ?>
+require 'header.php'; ?>
 <?php require 'neck.php'; ?>
 <?php require 'chest.php'; ?>
 <?php require 'abdomen.php'; ?>
